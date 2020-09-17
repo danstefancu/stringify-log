ff541dd90e14670eb9ecef615e8b32efe51210a6
Dan Stefancu <d.stefancu@rankingcoach.com>
2020-09-17 22:02:56 +0300

    New: option to exclude the stack from error objects.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,7 @@ import {stringify} from "../src";
 import {expect} from 'chai';
 
 describe('stringify', () => {
-    it('serializes object', (done) => {
+    it('should serialize objects', (done) => {
         let test = stringify({ prop: 'value'});
 
         expect(test).to.equal('{"prop":"value"}');
@@ -10,7 +10,7 @@ describe('stringify', () => {
         done();
     });
 
-    it('serializes arrays with length', (done) => {
+    it('should serialize arrays with length', (done) => {
         let test = stringify([1,2,3]);
 
         expect(test).to.include('3:[1,2,3]');
@@ -18,7 +18,7 @@ describe('stringify', () => {
         done();
     });
 
-    it('serializes errors with all props', (done) => {
+    it('should serialize errors with all props', (done) => {
         let test = stringify(new TypeError('hehe'));
 
         expect(test).to.include('hehe');
@@ -26,7 +26,15 @@ describe('stringify', () => {
         done();
     });
 
-    it('logs strings as they are', (done) => {
+    it('should serialize errors without stack for param', (done) => {
+        let test = stringify(new TypeError('hehe'), true);
+
+        expect(test).to.equal('{"message":"hehe"}');
+
+        done();
+    });
+
+    it('should serialize strings as they are', (done) => {
         let test = stringify('some string:');
 
         expect(test).to.include('some string:');
@@ -34,7 +42,15 @@ describe('stringify', () => {
         done();
     });
 
-    it('serialize everything else', (done) => {
+    it('should keep double quotes in strings', (done) => {
+        let test = stringify('some string"');
+
+        expect(test).to.include('some string"');
+
+        done();
+    });
+
+    it('should serialize everything else', (done) => {
         let test = stringify(42);
 
         expect(test).to.include('42');
